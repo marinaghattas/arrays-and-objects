edd4026e25aaf3af08dde024ea7e2170f5595e52
Marina Ghattas <marina.ghattas@f1006-11-34159.bmcc.cuny.edu>
2015-10-26 16:01:00 -0400

    create bubbles

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ function bubble(){
     this.x = random(width);
     this.y = random(height);
     this.r = 20; //r is radius which is 1/2 the width of the circle
-    this.bubbleSize = r*2; //width and height are twice the radius
+    this.bubbleSize = this.r*2; //width and height are twice the radius
 
-    //draw the bubble
+    //method: draw the bubble
+    this.drawBubble = function(){
     ellipse(this.x, this.y, this.bubbleSize, this.bubbleSize);
+    }
 } // end bubble
 
 
@@ -14,30 +16,29 @@ function bubble(){
 
 
 function setup(){
-    var cnv = createCanvas(800,3000);
+    var cnv = createCanvas(windowWidth,windowHeight);
     cnv.parent("windows");
 
     var myArray = []; // create a blank array
 
-    for(var i=0; i<10000; i++){
+    for(var i=0; i<10; i++){
 
-        myArray[i] = ellipse(i,i,10,10);
+        myArray[i] = new bubble();
+        myArray[i].drawBubble();
     }
 
     console.log(myArray);
 
     for(var i=0; i < myArray.length; i++){
-        myArray[i]+=10;
 
 
-       // console.log(myArray[i]);
-        console.log(myArray);
-                    }
-    console.log("myArray",myArray);
+        console.log(myArray[i]);
 
+    }
+    console.log(myArray);
+//   console.log("myArray",myArray);
 //    console.log(myArray[0]);
 //    console.log(myArray[1]);
 //    console.log(myArray[2]);
 //    console.log(myArray);
-//    console.log("myArray", myArray);
 } //end setup
