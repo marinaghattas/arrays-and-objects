a28e8a238a0f987d7ba505e7d72c37b83271d089
Marina Ghattas <marina.ghattas@f1006-11-34159.bmcc.cuny.edu>
2015-10-26 14:45:06 -0400

    entering basic array

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function setup(){
+    var cnv = createCanvas(800,3000);
+    cnv.parent("windows");
+
+    var myArray = ["hello",3,"bye"];
+    console.log(myArray[0]);
+    console.log(myArray[1]);
+    console.log(myArray[2]);
+    console.log(myArray);
+    console.log("myArray", myArray);
+} //end setup
